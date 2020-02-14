db4db64ce6b93296a71f607ae4af7ec6cfedb656
hmutschler15 <hmutschl@gmail.com>
2020-02-14 19:24:45 +0000

    File Structure

---
+++ cs/300/lab2/program1.c
@@ -0,0 +1,12 @@
+/* First RPi Program
+   Name: Hamilton Mutschler
+*/
+#include <stdio.h>
+
+int main(void)
+{
+   printf("Hello world.\n");
+   printf("Compiled and run on a RPi.\n");
+   printf("Edited via VSCode on Windows PC.\n");
+   return 0;
+}
