47f982f284f45690dddc0b8c378dd40534a8b221
hmutschler15 <hmutschl@gmail.com>
2020-02-26 19:11:55 +0000

    basic bt functionality working

--- engr/340/bt_testing.py
+++ engr/340/bt_testing.py
@@ -2,9 +2,31 @@
 
 # simple inquiry example
 import bluetooth
+import re
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print("Found {} devices.".format(len(nearby_devices)))
+bdAddr = "FC:58:FA:22:B4:C6"
 
-for addr, name in nearby_devices:
-    print("  {} - {}".format(addr, name))
+# nearby_devices = bluetooth.discover_devices(lookup_names=True)
+# print("Found {} devices.".format(len(nearby_devices)))
+
+# for addr, name in nearby_devices:
+#     print("  {} - {}".format(addr, name))
+
+# create a socket for connection to device
+socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+# socket.settimeout(10)
+# CHANNEL IS 1 TO MATCH BT DEVICE SERVER
+channel = 1
+# try to connect to device
+socket.connect((bdAddr, channel))
+print("Connection succeeded.")
+socket.send('AT+ADDR?\r\n')
+socket.settimeout(5)
+bdData = ''
+bdData = (socket.recv(2048))
+# decode utf
+decodedData = bdData.decode('utf-8', 'ignore')
+# decode any regular expression data
+ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+finalData = ansi_escape.sub('', decodedData)
+print(finalData)
