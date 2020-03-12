6536ac394fe6a1170391a41b385b2aeb88bccc0d
hmutschler15 <hmutschl@gmail.com>
2020-03-12 19:16:38 +0000

    finished cs 300 lab 5

--- cs/300/lab5/servo_switch_hw_pwm.py
+++ cs/300/lab5/servo_switch_hw_pwm.py
@@ -17,10 +17,23 @@ pi.set_mode(SWITCH, pigpio.INPUT)
 # set internal pulldown on switch input
 pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)
 # set debouncing value for switch
-pi.set_glitch_filter(SWITCH, 200)
+pi.set_glitch_filter(SWITCH, 300)
+
+# set initial PWM and ANGLE values
+MIN_PWM = 1000
+ZERO_PWM = 1500
+MAX_PWM = 2000
+MIN_ANGLE = -90
+MAX_ANGLE = 90
 
 # set initial state to wait for button
 STATE = "WAIT_FOR_BUTTON"
+
+# move the servo motor to the passed angle
+def move_to_angle(degrees):
+    PWM = degrees * (MAX_PWM - MIN_PWM) / (MAX_ANGLE - MIN_ANGLE) + ZERO_PWM
+    # set servo to random pwm value
+    pi.set_servo_pulsewidth(MOTOR, PWM)
 
 # callback function to handle behavior on a switch press
 def switch_press(gpio, level, tick):
@@ -46,16 +59,11 @@ try:
         # if state = 1, generate random movement of servo
         else:
             print("generating random movement")
-            RANDOM_PWM = randint(0,2)
-            if RANDOM_PWM == 0:
-                print('setting angle = -90 degrees')
-                pi.set_servo_pulsewidth(MOTOR, 1000)
-            elif RANDOM_PWM == 1:
-                print('setting angle = 0 degrees')
-                pi.set_servo_pulsewidth(MOTOR, 1500)
-            else:
-                print('setting angle = 90 degrees')
-                pi.set_servo_pulsewidth(MOTOR, 2000)
+            RANDOM_ANGLE = randint(MIN_ANGLE, MAX_ANGLE)
+            # move the servo motor to generated angle
+            move_to_angle(RANDOM_ANGLE)
+            print("moved to angle "+str(RANDOM_ANGLE))
+            # reset state to waiting for button
             STATE = "WAIT_FOR_BUTTON"
 except KeyboardInterrupt:
     pi.stop()
